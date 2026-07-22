1b6c9da6c6733292c13da9856c192d2d33f76faa
Arpit Tagade <143588157+tagadearpit@users.noreply.github.com>
2026-07-23 00:36:59 +0530

    Fix health check report data loss for empty results (#1)

--- krkn_ai/reporter/health_check_reporter.py
+++ krkn_ai/reporter/health_check_reporter.py
@@ -36,7 +36,7 @@ class HealthCheckReporter:
 
             for component_results in health_check_results:
                 if len(component_results) == 0:
-                    break
+                    continue
                 component_name = component_results[0].name
                 min_response_time = min(
                     [result.response_time for result in component_results]
